988703c9a257e3be82661ad7cd1f3b0579c99da8
Thanghv09 <126430044+Thanghv09@users.noreply.github.com>
2023-03-06 21:01:32 +0700

    Update merge character to map, bullet

---
+++ Assets/Script/bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float Damage = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float f=10;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.position.x - GameObject.Find("Target").transform.position.x, transform.position.y - GameObject.Find("Target").transform.position.y)*f;
+        Destroy(gameObject, 3f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
