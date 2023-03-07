b262da763637b47f70c84f5c9e1eb6f2e8a42dce
kiên <91230993+kienphan1234@users.noreply.github.com>
2023-03-07 16:22:49 +0700

    them thanh mau

---
+++ Assets/Script/HealthTank.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthTank : MonoBehaviour
+{
+	public Movie playerHealth;
+	public Image fillImage;
+	private Slider slider;
+
+	private void Awake()
+	{
+		slider = GetComponent<Slider>(); 
+	}
+
+	private void Update()
+	{
+
+		if (slider.value <= slider.minValue)
+		{
+			fillImage.enabled = false;
+		}
+		if (slider.value > slider.minValue && !fillImage.enabled)
+		{
+			fillImage.enabled = true;
+		}
+
+		
+		float fillValue = playerHealth.Current_Health / playerHealth.Max_Health;
+
+		if (fillValue <= slider.maxValue / 3)
+		{
+			fillImage.color = Color.white;
+		}
+		else if(fillValue > slider.maxValue / 3)
+		{
+			fillImage.color= Color.red;
+		}
+		slider.value = fillValue;
+
+
+	}
+}
